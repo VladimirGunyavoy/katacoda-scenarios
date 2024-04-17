7005ca99af3df22bbeffe585ccacea79ed57f422
BorisShrayner <42216254+BorisShrayner@users.noreply.github.com>
2024-04-17 11:37:46 +0300

    29
    Добавляем юнит-тест

---
+++ python1/assets/test_cases.py
@@ -0,0 +1,18 @@
+mport unittest
+from step0 import find_min  # Импортируем функцию из файла пользователя
+
+# Тесты для задания 0
+class TestAssignment0(unittest.TestCase):
+    def test_find_min(self):
+        self.assertEqual(find_min(1, 2), 1)
+        self.assertEqual(find_min(2, 1), 1)
+        self.assertEqual(find_min(-1, 1), -1)
+        # Добавить больше тестовых случаев по необходимости
+
+# Если у вас есть другие задания, добавьте классы для их тестов здесь
+
+if __name__ == '__main__':
+    unittest.main()
+
+
+#для запуска - python test_cases.py TestAssignment0
