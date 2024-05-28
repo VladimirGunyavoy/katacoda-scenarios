32327389ea2a117c83eb2c9c18e758e9b815d5f5
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-05-29 00:29:26 +0300

    a

--- python-demo/assets/step0.py
+++
@@ -1,8 +0,0 @@
-import datetime
-
-def t1():
-    #исправьте строчку ниже
-    day  = 'Day of week: ' + str(datetime.datetime.today().weekday())
-    return day
-
-print(t1())
--- python-demo/assets/step1.py
+++
@@ -1,6 +0,0 @@
-def find_min2(var_1, var_2, var_3):
-    #Вставьте свой код ниже
-    
-    return min
-
-print(find_min2(3, 5, -1))
