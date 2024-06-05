6a88587901725b7aff924e55703d48a4888769d5
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-06-05 20:01:55 +0300

    Update step0.py

---
+++ lesson 1 part 3/assets/step0.py
@@ -0,0 +1,12 @@
+coffee_price = 1.99
+print('стоимость кофе:', coffee_price)
+
+doughnut_price= 2.49
+print('стоимость пончика:', doughnut_price)
+
+cost = coffee_price + doughnut_price
+print('общая стоимость:', cost)
+
+discount = 0.87
+with_discount = cost - discount
+print('общая стоимость со скидкой:', cost)
