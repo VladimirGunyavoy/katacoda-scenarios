5bbbb8db896c6c117e88ac7c954a591c57b401f3
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-07-05 13:14:14 +0300

    add_test_framework

---
+++ framework_test/assets/test_cases1.py
@@ -0,0 +1,35 @@
+import pprint
+from checker import SberChecker
+from pathlib import Path
+import numpy as np
+import os
+import json
+
+os.chdir('framework_test/assets')
+print(os.getcwd())
+
+filename = 'step0.py'
+
+N = 10
+my_inputs = np.random.randint(0, 10, size=(N, 2)).tolist()
+
+my_tests = []
+for i in range(len(my_inputs)):
+    my_tests.append({'input': list(map(str, my_inputs[i])), 'output':[str(sum(my_inputs[i]))]})
+
+# print(my_tests)
+
+sber_checker = SberChecker(
+    filename=filename,
+    tests=my_tests
+)
+res = sber_checker.run()
+
+json_res = json.dumps(res, indent=4)
+print()
+print(type(json_res))
+print()
+
+pprint.pprint((json_res))
+
+
---
+++ framework_test/assets/test_sum.py
@@ -0,0 +1,24 @@
+import pprint
+from checker import SberChecker
+from pathlib import Path
+import numpy as np
+
+filename = 'step0.py'
+
+N = 10
+my_inputs = np.random.randint(0, 10, size=(N, 2)).tolist()
+
+my_tests = []
+for i in range(len(my_inputs)):
+    my_tests.append({'input': my_inputs[i], 'output':[str(sum(my_inputs[i]))]})
+
+print(my_tests)
+
+sber_checker = SberChecker(
+    filename=filename,
+    tests=my_tests
+)
+res = sber_checker.run()
+pprint.pprint(res)
+
+
