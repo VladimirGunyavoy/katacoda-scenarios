9bc1eb0ae807b56ad8b16ae013f0bf33fd385452
bendenko-v <bendenko.v@gmail.com>
2024-08-08 15:41:59 +0200

    Add Lesson 3

---
+++ lesson_3_conditions/assets/test_2.py
@@ -0,0 +1,26 @@
+import json
+
+from checker import SberChecker
+
+
+filename = 'step_2.py'
+
+my_tests = [
+    {
+        'input': [],
+        'output':["до оператора if", "после оператора if"]
+    },
+]
+
+sber_checker = SberChecker(
+    filename=filename,
+    tests=my_tests
+)
+res = sber_checker.run()
+
+json_res = json.dumps(res, indent=4, ensure_ascii=False)
+
+print(json_res)
+
+
+
