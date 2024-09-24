6f7ad34ed5e844637c40afa2cad08198d1082368
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-09-25 01:28:48 +0300

    a

--- lesson_2_5_numbers_operations/assets/step_5.py
+++ lesson_2_5_numbers_operations/assets/step_5.py
@@ -5,7 +5,4 @@ percent = ... # Ваш код
 finish = round(start * (1 + percent) ** years, 2)
 
 # вывод для проверки
-print('Начальный взнос:', start)
-print('Количество лет:', years)
-print('Процентная ставка:', percent)
 print('Итоговый баланс:', finish)
