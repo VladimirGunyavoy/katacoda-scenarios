9835a8ef775edf939a6aa3a9d1ee83cd3c723746
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-09-24 16:04:33 +0300

    a

--- lesson_2_2_float/assets/step_4.py
+++ lesson_2_2_float/assets/step_4.py
@@ -4,14 +4,15 @@ print('стоимость кофе:', coffee_price)
 doughnut_price = 2.49
 print('стоимость пончика:', doughnut_price)
 
-discount = 0.87
-print('скидка: ', discount)
-
 cost = coffee_price + doughnut_price 
 print('общая стоимость:', cost)
+
+discount = 0.87
+print('скидка:', discount)
 
 cost -= discount
 cost = round(cost, 2)
 print('общая стоимость со скидкой:', cost)
 
 # ваш код
+...
