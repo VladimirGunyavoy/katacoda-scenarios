c7d4c3b194a711b99e664a3706a9572cd67869ec
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-09-26 04:11:54 +0300

    a

--- lesson_3_6_comparison_operators/assets/step_2.py
+++ lesson_3_6_comparison_operators/assets/step_2.py
@@ -1,10 +1,9 @@
 # темная магия для получения броска кубика
 # и вывода значения
 # ничего не меняйте
-import numpy as np
-dice1 = np.random.randint(1, 7)
-dice2 = np.random.randint(1, 7)
-print('dice 1:', dice1)
+import random
+dice1 = bool(random.randint(1, 6))
+dice2 = bool(random.randint(1, 6))
 print('dice 2:', dice2)
 print()
 
