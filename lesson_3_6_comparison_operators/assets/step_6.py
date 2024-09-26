e8097fbaa9e2786ed7c99c5fa5372221ffc9d368
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-09-26 04:13:19 +0300

    a

--- lesson_3_6_comparison_operators/assets/step_6.py
+++ lesson_3_6_comparison_operators/assets/step_6.py
@@ -2,8 +2,8 @@
 # и вывода значения
 # ничего не меняйте
 import random
-dice1 = bool(random.randint(1, 6))
-dice2 = bool(random.randint(1, 6))
+dice1 = random.randint(1, 6)
+dice2 = random.randint(1, 6)
 print('dice 1:', dice1)
 print('dice 2:', dice2)
 print()
