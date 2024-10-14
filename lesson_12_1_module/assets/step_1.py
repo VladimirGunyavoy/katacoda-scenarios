0b181c11d430f28a337a3c6957a007f9bfd8b2f1
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-10-14 16:52:36 +0300

    a

--- lesson_12_1_module/assets/step_1.py
+++ lesson_12_1_module/assets/step_1.py
@@ -1,10 +1,8 @@
 import csv
 
-print('hoi')
-
 with open('data1.csv', mode ='r')as file:
   csvFile = csv.reader(file)
   for lines in csvFile:
         s = sum(map(int, lines)) / len(lines)
 
-print(s)
+print(round(s, 2))
--- lesson_12_1_module/assets/test_1.py
+++ lesson_12_1_module/assets/test_1.py
@@ -1,6 +1,7 @@
 import os
 import json
 import re
+import random
 
 from checker import SberChecker
 
@@ -8,27 +9,60 @@ index = re.search(r'\d+', os.path.basename(__file__)).group()
 
 filename = f'filename.py'
 
-my_tests = [
-    {
-        "input": [],
-        "output": ["52.7", "5.0"]
-    },
+nums_1 = random.sample(range(1, 50), 5)
+res_1 = round(sum(nums_1), 2) / len(nums_1)
 
+nums_2 = random.sample(range(1, 50), 5)
+res_2 = round(sum(nums_2), 2) / len(nums_2)
+
+nums_3 = random.sample(range(1, 50), 5)
+res_3 = round(sum(nums_3), 2) / len(nums_3)
+
+my_tests_1 = [
+    {
+        "input": ['data_1.csv'],
+        "output": ['52.67']
+    }
+]
+
+my_tests_2 = [
+    {
+        "input": [nums_1],
+        "output": [res_1]
+    },
+    {
+        "input": [nums_2],
+        "output": [res_2]
+    },
+    {
+        "input": [nums_3],
+        "output": [res_3]
+    },
 ]
 
 postcode = """\n
-from my_module import my_mean
-
-print(my_mean([2, 4, 6, 8]))
+inp = input()
+print(my_mean(inp))
 """
 
-sber_checker = SberChecker(
+sber_checker_1 = SberChecker(
     filename=filename,
-    tests=my_tests,
+    tests=my_tests_1,
+)
+
+sber_checker_2 = SberChecker(
+    filename='my_module.py',
+    tests=my_tests_2,
     postcode=postcode,
 )
-res = sber_checker.run()
+
+res_1 = sber_checker_1.run()
+res_1['Test 0'] = res_1.pop('Test 1')
+res_2 = sber_checker_2.run()
+
+res = res_1 | res_2
 
 json_res = json.dumps(res, indent=4, ensure_ascii=False)
 
 print(json_res)
+# 
