c314b3abeab3672a147b326f289f73382859d4ab
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-10-23 15:53:33 +0300

    Update test_1.py

--- lesson_12_1_module/assets/test_1.py
+++ lesson_12_1_module/assets/test_1.py
@@ -63,7 +63,7 @@ sber_checker_1 = SberChecker(
 
 sber_checker_2 = SberChecker(
     filename='/usr/local/lib/empty.py',
-    filename='empty.py',
+    # filename='empty.py',
     tests=my_tests_2,
     postcode=postcode,
 )
