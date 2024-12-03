e179c70fdd77c0b3d490c6478acabc9e32577781
VladimirGunyavoy <34680650+VladimirGunyavoy@users.noreply.github.com>
2024-12-03 16:19:27 +0300

    Update test_2.py

--- lesson_9_5_functions_corner_cases/assets/test_2.py
+++ lesson_9_5_functions_corner_cases/assets/test_2.py
@@ -40,4 +40,37 @@ res = sber_checker.run()
 
 json_res = json.dumps(res, indent=4, ensure_ascii=False)
 
+dct = json.loads(json_res)
+
+for test in dct:
+    print(test)
+    print(dct[test])
+    print()
+    for res in dct[test]:
+        if res == 'input':
+            
+            # print(res)
+            dct[test][res][0] = str(dct[test][res][0])
+            
+        # print(res)
+        if res == 'expected':
+            # print(res)
+            # dct[test][res] = str(dct[test][res])
+            # print(dct[test][res])
+            
+            dct[test][res][0] = str(dct[test][res][0])
+            print(dct[test][res])
+            # print(dct[test][res])
+        
+            # print(dct[test][res])
+        
+        
+        # for el in dct[test][res]:
+        #     print(el)
+# print(json_res)
+
+print(dct)
+
+json_res = json.dumps(dct, indent=4, ensure_ascii=False)
+
 print(json_res)
